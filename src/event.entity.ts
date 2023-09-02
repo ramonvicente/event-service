6a35f65c370808c714b9fe605075ad637757dfda
Ramon Vicente <ramonvicentee@gmail.com>
2023-09-02 20:22:20 +0200

    add event entity to application

--- src/event.entity.ts
+++ src/event.entity.ts
@@ -1,7 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('events')
 export class Event {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column({ name: 'when_date' })
   when: Date;
+
+  @Column()
   description: string;
+
+  @Column()
   address: string;
 }
